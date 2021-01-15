6294f0369279bc286e0f9021f81910bd2cd6fb93
akaShivam <41604527+akaShivam@users.noreply.github.com>
2021-01-15 07:48:31 +0530

    Update github_stats.py

--- github_stats.py
+++ github_stats.py
@@ -249,6 +249,7 @@ class Stats(object):
         
         self.count_open_source_projects = 0
         self._count_stats_from_contributed = False
+        print(type(os.getenv("COUNT_STATS_FROM_CONTIRBUTED");
         if os.getenv("COUNT_STATS_FROM_CONTRIBUTED") != None:
             self._count_stats_from_contributed = True
         
